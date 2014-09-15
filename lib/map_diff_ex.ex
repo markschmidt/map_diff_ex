9ba4c09db7a9cd307efb6197c3e654792d2814c0
Mark Schmidt <mark.schmidt@xing.com>
2014-09-15 21:06:28 +0200

    added first rough version

--- lib/map_diff_ex.ex
+++ lib/map_diff_ex.ex
@@ -1,2 +1,24 @@
 defmodule MapDiffEx do
+
+  def diff(map1, map2), do: do_diff(map1, map2)
+
+  defp do_diff(map1, map2) when map1 == map2, do: nil
+  defp do_diff(map1, map2) when is_map(map1) and is_map(map2) do
+    map1
+    |> Map.keys
+    |> Enum.concat(Map.keys(map2))
+    |> Enum.map(fn key ->
+      {key, do_diff(Map.get(map1, key), Map.get(map2, key))}
+    end)
+    |> to_map
+  end
+
+  defp do_diff(value1, value2) do
+    {value1, value2}
+  end
+
+
+  defp to_map(list) do
+    Dict.merge(%{}, list)
+  end
 end
--- test/map_diff_ex_test.exs
+++ test/map_diff_ex_test.exs
@@ -1,7 +1,41 @@
 defmodule MapDiffExTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import MapDiffEx, only: [diff: 2]
+
+  test "should return nil for empty maps" do
+    assert diff(%{}, %{}) == nil
+  end
+
+  test "should return nil for non-empty but equal maps" do
+    map = %{a: 1, b: "test", "d": :a}
+    assert diff(map, map) == nil
+  end
+
+  test "should return a map with all the diffs for keys which are different" do
+    map1 = %{a: 1, b: "test", c: 0, "d": :a}
+    map2 = %{a: 2, b: "foobar", "d": :x}
+
+    expected_diff = %{a: {1, 2}, b: {"test", "foobar"}, c: {0, nil}, "d": {:a, :x}}
+
+    assert diff(map1, map2) == expected_diff
+  end
+
+  test "should also detect missing keys in the first hash" do
+    map2 = %{a: 1, b: "test", c: 0, "d": :a}
+    map1 = %{a: 2, b: "foobar", "d": :x}
+
+    expected_diff = %{a: {2, 1}, b: {"foobar", "test"}, c: {nil, 0}, "d": {:x, :a}}
+
+    assert diff(map1, map2) == expected_diff
+  end
+
+  test "should handle sub maps" do
+    map1 = %{a: %{b: 2}}
+    map2 = %{a: %{b: 3}}
+
+    expected_diff = %{a: %{b: {2,3}}}
+
+    assert diff(map1, map2) == expected_diff
   end
 end
